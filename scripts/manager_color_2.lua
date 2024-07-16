40b8dffa6cd09e63b8e9e141264e76eaa1aa6a2b
JustinFreitas <justin.s.freitas@me.com>
2024-07-15 19:59:18 -0600

    v2.9.30... fix for holiday day font.

--- scripts/manager_color_2.lua
+++ scripts/manager_color_2.lua
@@ -50,7 +50,7 @@ function onInit()
 	ColorManager.COLOR_PRIMARY_FOREGROUND = "FFF0CA";  -- Replaceable Color: Primary Foreground
 	ColorManager.COLOR_SECONDARY_FOREGROUND = "DDDDDD"; -- Replaceable Color: Secondary Foreground
 	ColorManager.COLOR_CALENDAR_BACKGROUND = "473C33"; -- Replaceable Color: Calendar Background
-	ColorManager.COLOR_CALENDAR_HOLIDAY = "F2AB76 "; -- Replaceable Color: Calendar Holiday
+	ColorManager.COLOR_CALENDAR_HOLIDAY = "F2AB76"; -- Replaceable Color: Calendar Holiday
 	ColorManager.COLOR_CALENDAR_MONTH = "FF6161"; -- Replaceable Color: Calendar Month
 
 	ColorManager.COLOR_MODIFIED_VALUE = "F3D876"; -- Replacable Color: Modified VALUE
